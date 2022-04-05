d6252565ce4033c9471367037b346787601837d1
Rafael Rodrigues <rafael.r.silva1996@gmail.com>
2022-04-05 20:27:14 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://theme.zdassets.com/theme_assets/830444/b5e197fff076818dbb2e720df677cb40cbf05677.png" alt="Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Usuário" />
+                </a>
+            </div>
+        </header>
+    )
+}
